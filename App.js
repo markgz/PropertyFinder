c468d62af5369eb7b1c6fce93e23d78928659c31
markyan <mark.yangz@ef.com>
2018-05-18 15:54:50 +0800

    Search Page

--- App.js
+++ App.js
@@ -6,31 +6,29 @@
 'use strict';
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS,
 } from 'react-native';
+import SearchPage from './SearchPage';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component<{}> {
   render() {
-    return React.createElement(Text, {style: styles.description}, "Search for houses to buy!");
+    return (
+      <NavigatorIOS
+        sytle={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
   }
 }
 
+
 const styles = StyleSheet.create({
-  description: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65,
+  container: {
+    flex: 1,
   },
 });
